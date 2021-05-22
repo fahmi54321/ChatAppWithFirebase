41452bda884f4411d17de66e435c346972e56d0b
fahmi54321 <fahmiabdulaziz44@gmail.com>
2021-05-22 13:06:14 +0700

    home screen

---
+++ app/src/main/java/com/android/chatappwithfirebase/Fragments/ChatViewModel.java
@@ -0,0 +1,7 @@
+package com.android.chatappwithfirebase.Fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class ChatViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/android/chatappwithfirebase/Fragments/PeopleViewModel.java
@@ -0,0 +1,7 @@
+package com.android.chatappwithfirebase.Fragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class PeopleViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
